f90904a7f7e50f4f133bc299829713c570124aa0
lucac <luca.corinzia@inf.ethz.ch>
2019-03-21 11:41:07 +0100

    deterministic net

---
+++ source/deterministic_net.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+import os
+import GPUtil
+import keras
+
+def model_loss(model):
+
+    def bbp_loss(y_true, y_pred):
+        neg_log_likelihood = tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+        kl = sum(model.losses)
+        loss = neg_log_likelihood + kl/60000*128
+        return loss + kl
+
+    return bbp_loss
+
+
+def set_free_gpus(num):
+    # num: integer; number of GPUs that shall be allocated
+    # returns: string; listing a total of 'num' available GPUs.
+
+    list_gpu = GPUtil.getAvailable(limit=num, maxMemory=0.02)
+    return str(list_gpu)[1:-1]
+
+
+NUM_GPUS = 1
+
+if NUM_GPUS > 0:
+    os.environ["CUDA_DEVICE_ORDER"]='PCI_BUS_ID'
+    os.environ["CUDA_VISIBLE_DEVICES"] = set_free_gpus(NUM_GPUS)
+    print(os.environ["CUDA_VISIBLE_DEVICES"])
+    distribution = tf.contrib.distribute.MirroredStrategy(num_gpus=NUM_GPUS)
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    tf.keras.backend.set_session(sess)
+
+num_classes = 10
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 126
+x_test /= 126
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+model = tf.keras.Sequential([
+  tf.layers.Dense(400, activation='relu'),
+  tf.layers.Dense(400, activation='relu'),
+  tf.layers.Dense(10, activation='softmax'),
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(0.001),
+              loss=tf.keras.losses.categorical_crossentropy,
+              metrics=['accuracy'])
+
+model.fit(x=x_train, y=y_train, epochs=100, batch_size=128)
+model.evaluate(x_test, y_test)
