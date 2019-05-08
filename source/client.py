fixed minus sign in scale calculation

source/client.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 from tensorflow_probability.python.layers import DenseReparameterization
-from utils import get_posterior_from_layer, clone, get_refined_prior, gaussian_ratio_par
+from utils import get_posterior_from_layer, get_refined_prior, gaussian_ratio_par, LateralConnection, prior_wrapper
 import tensorflow_probability as tfp
+from utils import softminus
+import numpy as np
 
 
 class _Client(tf.keras.Model):
@@ -10,28 +12,46 @@ class _Client(tf.keras.Model):
         super(_Client, self).__init__(*args, **kwargs)
         self.prior_fn_server = get_refined_prior
         self.prior_fn_client = None
-        self.old_server_par = {}
+        self.q = None
+        self.t = None
         self.data_set_size = None
         self.n_samples = None
 
-    def update_prior(self, client_refining=False, data_set=None):
+    def update_prior(self, client_refining=False):
         if client_refining:
             self.prior_fn_client = lambda l: tfp.layers.default_multivariate_normal_fn
-            if data_set not in self.old_server_par:
-                self.old_server_par[data_set] = None
         else:
             self.prior_fn_client = get_posterior_from_layer
 
         for layer in self.layers:
-            if issubclass(layer.__class__, DenseReparameterization):
+            if issubclass(layer.__class__, DenseReparameterization) or isinstance(layer, LateralConnection):
                 if '_client_' not in layer.name:
-                    old_weights = None
+                    t = None
                     if client_refining:
-                        if self.old_server_par[data_set]:
-                            old_weights = self.old_server_par[data_set][layer.name]
-                    layer.update_prior(self.prior_fn_server(layer, old_weights))
+                        if self.t:
+                            t = self.t[layer.name]
+                    layer.update_prior(self.prior_fn_server(layer, t))
                 else:
-                    layer.update_prior(self.prior_fn_client(layer))
+                    layer.update_prior(prior_wrapper(self.prior_fn_client, layer))
+
+    def new_t(self, server):
+        if self.q:
+            self.t = {layer.name: gaussian_ratio_par(layer.get_weights(), self.q[layer.name])
+                      for layer in server.layers}
+        else:
+            self.t = {}
+            for layer_name, layer_weight in server.get_dict_weights().items():
+                if len(layer_weight) > 1:
+                    standard_normal = [np.zeros_like(layer_weight[0]),
+                                       softminus(np.ones_like(layer_weight[1]))]
+                    self.t[layer_name] = gaussian_ratio_par(
+                        layer_weight,
+                        standard_normal)
+                else:
+                    self.t[layer_name] = []
+
+    def set_q(self):
+        self.q = {layer.name: layer.get_weights() for layer in self.layers if '_client_' not in layer.name}
 
 
 class Client(tf.keras.Model):
@@ -59,16 +79,38 @@ class Client(tf.keras.Model):
         output = tf.keras.layers.Lambda(lambda q: tf.reduce_sum(q, axis=0))(output)
         return output
 
-    def update_prior(self, client_refining=False, data_set=None):
-        self.model.update_prior(client_refining, data_set)
+    def update_prior(self, client_refining=False):
+        self.model.update_prior(client_refining)
+
+    def new_t(self, server):
+        self.model.new_t(server)
+
+    def set_q(self):
+        self.model.set_q()
 
     @property
     def data_set_size(self):
         return self.model.data_set_size
 
     @property
-    def old_server_par(self):
-        return self.model.old_server_par
+    def layers(self):
+        return self.model.layers
+
+    @property
+    def t(self):
+        return self.model.t
+
+    @property
+    def q(self):
+        return self.model.q
+
+    @t.setter
+    def t(self, t):
+        self.model.t = t
+
+    @q.setter
+    def q(self, q):
+        self.model.q = q
 
     @data_set_size.setter
     def data_set_size(self, a):

source/federated_exp.py
@@ -1,110 +1,146 @@
 import tensorflow as tf
-from utils import gpu_session, permuted_mnist_for_n_tasks, get_mlp_server, femnist_data, DenseReparameterizationPriorUpdate
+from utils import DenseReparameterizationPriorUpdate
+from general_utils import gpu_session, get_mlp_server, aulc
+from data_utils import permuted_mnist, femnist, mnist
 from network_manager import NetworkManager
 import numpy as np
-import pickle
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from sacred.utils import apply_backspaces_and_linefeeds
 
-NUM_GPUS = 1
-NUM_TASKS = 5
-NUM_EPOCHS = 10000
-BATCH_SIZE = 32
-NUM_REP = 5
-DATA_SET_SIZE_PER_USER = 250
-TEST_SET_SIZE_PER_USER = 25
-NUM_SAMPLES = 10
-NUM_REFINEMENT = 2
-INPUT_SHAPE = (784, )
-LAYER = DenseReparameterizationPriorUpdate
-LAYER_UNITS = [100, 100, 62]
-ACTIVATIONS = ['relu', 'relu', 'softmax']
-LEARNING_RATE = 0.001
-PATIENCE = 100
-VALIDATION_SPLIT = 0.2
-TRAIN_SET_SIZE_PER_USER = int(DATA_SET_SIZE_PER_USER*(1-VALIDATION_SPLIT))
-DATA_SET_GENERATOR = femnist_data
-VERBOSE = 0
+ex = Experiment("experiments")
 
-config = gpu_session(num_gpus=1)
-if config:
-    sess = tf.Session(config=config)
-    tf.keras.backend.set_session(sess)
+ex.observers.append(
+    FileStorageObserver.create("runs")
+)
 
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=PATIENCE)
+ex.captured_out_filter = apply_backspaces_and_linefeeds
 
-accuracy_virtual = []
-history_overall = []
+@ex.config
+def cfg():
+    session = {
+        "num_gpus": 0,
+        "gpus": None,
+        "experiment_name": None
+    }
+    data_set = {
+        "num_tasks": 0,
+        "data_set_size_per_user": 0,
+        "test_set_size_per_user": 0,
+        "generator": None
+    }
+    training = {
+        "num_epochs": 0,
+        "batch_size": 0,
+        "num_refining": 0,
+        "patience": 0,
+        "learning_rate": 0,
+        "validation_split": 0,
+        "verbose": 0
+    }
+    model = {
+        "num_samples": 0,
+        "input_shape": [],
+        "layer": None,
+        "layer_units": [],
+        "activations": []
+    }
+    data_set['train_set_size_per_user'] = int(data_set['data_set_size_per_user']*(1-training['validation_split']))
+    data_set['global_size'] = data_set['train_set_size_per_user']*data_set['num_tasks']
 
-x, y, x_t, y_t = DATA_SET_GENERATOR(NUM_TASKS, global_data=False,
-                              train_set_size_per_user=DATA_SET_SIZE_PER_USER,
-                              test_set_size_per_user=TEST_SET_SIZE_PER_USER)
 
-network_m = NetworkManager(
-    get_mlp_server(INPUT_SHAPE, LAYER, LAYER_UNITS, ACTIVATIONS, TRAIN_SET_SIZE_PER_USER, NUM_SAMPLES),
-                        data_set_size=TRAIN_SET_SIZE_PER_USER, n_samples=NUM_SAMPLES)
-clients = network_m.create_clients(NUM_TASKS)
-network_m.compile(optimizer=tf.keras.optimizers.Adam(LEARNING_RATE),
-                  loss=tf.keras.losses.categorical_crossentropy,
-                  metrics=[tf.keras.metrics.categorical_accuracy])
-model_sequence = np.tile(range(NUM_TASKS), NUM_REFINEMENT)
-data_sequence = np.tile(range(NUM_TASKS), NUM_REFINEMENT)
-history_virtual, evaluation_virtual = network_m.fit(model_sequence, data_sequence,
-                        x=x, y=y, validation_split=VALIDATION_SPLIT,
-                        epochs=NUM_EPOCHS, test_data=zip(x_t, y_t), batch_size=BATCH_SIZE,
-                        callbacks=[early_stopping], verbose=VERBOSE
-                        )
+@ex.capture
+def get_globals(data_set, model):
+    model['layer'] = globals()[model['layer']]
+    data_set['generator'] = globals()[data_set['generator']]
 
-print('virtual done')
-with open('femnist_virtual_history.pkl', 'wb') as f:
-    pickle.dump(history_virtual, f)
 
-with open('femnist_virtual_eval.pkl', 'wb') as f:
-    pickle.dump(evaluation_virtual, f)
+@ex.automain
+def run(session, data_set, training, model):
+    get_globals()
+    config = gpu_session(num_gpus=session['num_gpus'], gpus=session['gpus'])
+    if config:
+        sess = tf.Session(config=config)
+        tf.keras.backend.set_session(sess)
 
-history_local = {}
-evaluation_local = {}
-for n in range(NUM_TASKS):
-    print('local model number ', n)
-    single_model = get_mlp_server(INPUT_SHAPE, LAYER, LAYER_UNITS, ACTIVATIONS, TRAIN_SET_SIZE_PER_USER, NUM_SAMPLES)
-    single_model.compile(optimizer=tf.keras.optimizers.Adam(LEARNING_RATE),
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=training['patience'])
+
+    x, y, x_t, y_t = data_set['generator'](data_set['num_tasks'], global_data=False,
+                                  train_set_size_per_user=data_set['data_set_size_per_user'],
+                                  test_set_size_per_user=data_set['test_set_size_per_user'])
+
+    history_local = {}
+    evaluation_local = {}
+
+    network_m = NetworkManager(
+        get_mlp_server(model['input_shape'], model['layer'], model['layer_units'],
+                       model['activations'], data_set['train_set_size_per_user'], model['num_samples']),
+                            data_set_size=data_set['train_set_size_per_user'], n_samples=model['num_samples'])
+    network_m.create_clients(data_set['num_tasks'])
+    network_m.compile(optimizer=tf.keras.optimizers.Adam(training['learning_rate']),
                       loss=tf.keras.losses.categorical_crossentropy,
                       metrics=[tf.keras.metrics.categorical_accuracy])
-    history_local[n] = single_model.fit(x=x[n], y=y[n], validation_split=VALIDATION_SPLIT,
-                        epochs=NUM_EPOCHS, batch_size=BATCH_SIZE,
-                        callbacks=[early_stopping], verbose=VERBOSE).history
-    evaluation_local[n] = single_model.evaluate(x_t[n], y_t[n])
-    print(evaluation_local[n])
+    sequence = np.tile(range(data_set['num_tasks']), training['num_refining'])
+    history_virtual, evaluation_virtual = network_m.fit(sequence,
+                            x=x, y=y, validation_split=training['validation_split'],
+                            epochs=training['num_epochs'], test_data=zip(x_t, y_t), batch_size=training['batch_size'],
+                            callbacks=[early_stopping], verbose=training['verbose']
+                            )
 
-with open('femnist_local_history.pkl', 'wb') as f:
-    pickle.dump(history_local, f)
+    print('virtual done')
 
-with open('femnist_local_eval.pkl', 'wb') as f:
-    pickle.dump(evaluation_local, f)
+    for n in range(data_set['num_tasks']):
+        print('local model number ', n)
+        network_m = NetworkManager(get_mlp_server(model['input_shape'], model['layer'], model['layer_units'],
+                                                  model['activations'], data_set['train_set_size_per_user'],
+                                                  model['num_samples']),
+                                   data_set_size=data_set['train_set_size_per_user'], n_samples=model['num_samples'])
+        single_model = network_m.create_clients(1)[0]
+        single_model.compile(optimizer=tf.keras.optimizers.Adam(training['learning_rate']),
+                             loss=tf.keras.losses.categorical_crossentropy,
+                             metrics=[tf.keras.metrics.categorical_accuracy])
+        history_local[n] = single_model.fit(x=x[n], y=y[n], validation_split=training['validation_split'],
+                                            epochs=training['num_epochs'], batch_size=training['batch_size'],
+                                            callbacks=[early_stopping], verbose=training['verbose']).history
+        evaluation_local[n] = single_model.evaluate(x_t[n], y_t[n])
+        print(evaluation_local[n])
 
-x = np.concatenate(x)
-y = np.concatenate(y)
-x_t = np.concatenate(x_t)
-y_t = np.concatenate(y_t)
+    x = np.concatenate(x)
+    y = np.concatenate(y)
+    x_t = np.concatenate(x_t)
+    y_t = np.concatenate(y_t)
 
-DATA_SET_SIZE = TRAIN_SET_SIZE_PER_USER*NUM_TASKS
-global_net = get_mlp_server(INPUT_SHAPE, LAYER, LAYER_UNITS, ACTIVATIONS, DATA_SET_SIZE, NUM_SAMPLES)
-global_net.compile(optimizer=tf.keras.optimizers.Adam(LEARNING_RATE),
-                  loss=tf.keras.losses.categorical_crossentropy,
-                  metrics=[tf.keras.metrics.categorical_accuracy])
-print('starting global model')
-history_global = global_net.fit(x=x,
-                         y=y,
-                         epochs=NUM_EPOCHS,
-                         validation_split=VALIDATION_SPLIT,
-                         batch_size=BATCH_SIZE,
-                         callbacks=[early_stopping], verbose=VERBOSE
-                         ).history
-evaluation_global = global_net.evaluate(x_t, y_t)
-print(evaluation_global)
+    global_net = get_mlp_server(model['input_shape'], model['layer'], model['layer_units'],
+                       model['activations'], data_set['global_size'], model['num_samples'])
+    global_net.compile(optimizer=tf.keras.optimizers.Adam(training['learning_rate']),
+                      loss=tf.keras.losses.categorical_crossentropy,
+                      metrics=[tf.keras.metrics.categorical_accuracy])
+    print('starting global model')
+    history_global = global_net.fit(x=x,
+                             y=y,
+                             epochs=training['num_epochs'],
+                             validation_split=training['validation_split'],
+                             batch_size=training['batch_size'],
+                             callbacks=[early_stopping], verbose=training['verbose']
+                             ).history
+    evaluation_global = global_net.evaluate(x_t, y_t)
 
-with open('femnist_global_history.pkl', 'wb') as f:
-    pickle.dump(history_global, f)
+    local_average = np.array(list(evaluation_local.values())).mean(axis=0)
+    virtual_average = np.array(list(evaluation_virtual.values())).mean(axis=(0, 1))
+    normalized_accuracy = np.array([(np.array(v)/np.array(l)) for (v, l) in
+                           zip(list(evaluation_virtual.values()), list(evaluation_local.values()))])[:, :, 1]
+    aulcs = aulc(history_virtual)
 
-with open('femnist_global_eval.pkl', 'wb') as f:
-    pickle.dump(evaluation_global, f)
+    ex.info['virtual_history'] = history_virtual
+    ex.info['virtual_evaluation'] = evaluation_virtual
+    ex.info['local_history'] = history_local
+    ex.info['local_evaluation'] = evaluation_local
+    ex.info['global_history'] = history_global
+    ex.info['global_evaluation'] = evaluation_global
+    ex.info['local_average'] = local_average
+    ex.info['virtual_average'] = virtual_average
+    ex.info['aulc'] = aulcs
+    ex.info['normalized_virtual_accuracy'] = normalized_accuracy
 
-print('done')
+    print('done')
+    return evaluation_virtual, evaluation_local, evaluation_global

source/network_manager.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
-from utils import get_posterior_from_layer, clone, get_refined_prior, gaussian_ratio_par, Gate
+from utils import get_posterior_from_layer, get_refined_prior, gaussian_ratio_par, Gate, LateralConnection
+from general_utils import clone
 from tensorflow.python.keras.engine.input_layer import InputLayer
 from client import Client
 from collections import defaultdict
+import numpy as np
 
 
 class NetworkManager:
@@ -19,7 +21,7 @@ class NetworkManager:
         self.optimizer = dict(tf.keras.optimizers.serialize(optimizer))
         self.compile_conf = kwargs
 
-    def fit(self, model_sequence, data_sequence, **kwargs):
+    def fit(self, sequence, **kwargs):
         x = kwargs.pop('x')
         y = kwargs.pop('y', None)
         validation_data = kwargs.pop('validation_data', None)
@@ -29,34 +31,32 @@ class NetworkManager:
         if test_data:
             test_data = list(test_data)
         optimizer = tf.keras.optimizers.deserialize(dict(self.optimizer))
-        sequence = list(zip(model_sequence, data_sequence))
-        refined = list(set([x for x in sequence if sequence.count(x) >= 2]))
+        unique, counts = np.unique(sequence, return_counts=True)
+        counts = dict(zip(unique, counts))
+        refined = [counts[u] > 1 for u in unique]
         history = defaultdict(list)
         evaluate = defaultdict(list)
-        for t, (i, j) in enumerate(sequence):
-            print('step ', t, ' in sequence of lenght ', len(sequence), ' task ', i)
-            self.clients[i].update_prior(client_refining=((i, j) in refined), data_set=j)
+        for t, i in enumerate(sequence):
+            print('step ', t+1, ' in sequence of lenght ', len(sequence), ' task ', i+1)
+            if t > 0:
+                self.clients[i].update_prior(client_refining=refined[i])
+                self.clients[i].set_q()
             self.clients[i].compile(optimizer, **self.compile_conf)
-            fit_config = {'x': x[j]}
+            fit_config = {'x': x[i]}
             if y:
-                fit_config['y'] = y[j]
+                fit_config['y'] = y[i]
             if validation_data:
-                fit_config['validation_data'] = validation_data[j]
+                fit_config['validation_data'] = validation_data[i]
             fit_config.update(kwargs)
             hist = self.clients[i].fit(**fit_config)
             history[i].append(hist.history)
             if test_data:
-                eval = self.clients[i].evaluate(*test_data[j])
+                eval = self.clients[i].evaluate(*test_data[i])
                 evaluate[i].append(eval)
                 print(eval)
-            if (i, j) in refined:
-                if self.clients[i].old_server_par[j]:
-                    for layer in self.clients[i].old_server_par[j]:
-                        self.clients[i].old_server_par[j][layer] = gaussian_ratio_par(
-                                                self.server.get_layer(layer).get_weights(),
-                                                self.clients[i].old_server_par[j][layer])
-                else:
-                    self.clients[i].old_server_par[j] = self.server.get_dict_weights()
+            if refined[i]:
+                self.clients[i].new_t(self.server)
+
         return history, evaluate
 
     def create_clients(self, num_clients):
@@ -71,22 +71,14 @@ class NetworkManager:
         input_tensor = server.input
         x = input_tensor
         server.client_count += 1
-        name = '_client_' + str(server.client_count)
+        name_suffix = '_client_' + str(server.client_count)
         for layer in server.layers:
             if not isinstance(layer, InputLayer):
                 if 'lateral' in layer.name:
-                    out1 = clone(layer, data_set_size=self.data_set_size, n_samples=self.n_samples,
-                                 activation='linear', name=name + '_from_client')(x)
-                    out2 = clone(layer, data_set_size=self.data_set_size, n_samples=self.n_samples,
-                                 name=name + '_from_server1')(layer.output)
-                    out2 = clone(layer, data_set_size=self.data_set_size, n_samples=self.n_samples,
-                                 activation='linear', name=name + '_from_server2')(out2)
-                    out2 = Gate()(out2)
-                    x = tf.keras.layers.add([out1, out2], name=layer.name + '_add' + name)
-                    x = tf.keras.layers.Activation(layer.get_config()['activation'],
-                                                   name=layer.name + '_activation' + name)(x)
+                    name = layer.name + name_suffix
+                    x = LateralConnection(layer, self.data_set_size, self.n_samples, name=name)([x, layer.output])
                 else:
-                    x = clone(layer, data_set_size=self.data_set_size, n_samples=self.n_samples, name=name)(x)
+                    x = clone(layer, data_set_size=self.data_set_size, n_samples=self.n_samples, name=name_suffix)(x)
         client = Client(input_tensor, x, n_samples=self.n_samples)
         client.data_set_size = self.data_set_size
         return client

source/utils.py
@@ -1,13 +1,10 @@
 import tensorflow as tf
-import os
 import tensorflow_probability as tfp
 import numpy as np
-import GPUtil
 from tensorflow_probability.python import  distributions as tfd
-from tensorflow_probability.python.layers import DenseReparameterization
-from server import Server
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.keras import initializers
+from general_utils import clone
 
 
 class Gate(tf.keras.layers.Layer):
@@ -63,36 +60,50 @@ class DenseReparameterizationPriorUpdate(tfp.layers.DenseReparameterization):
                                name='divergence_kernel')
 
 
-def gpu_session(num_gpus=None, gpus=None):
-    if gpus:
-        os.environ["CUDA_VISIBLE_DEVICES"] = gpus
-    if num_gpus:
-        if num_gpus >0:
-            os.environ["CUDA_DEVICE_ORDER"]='PCI_BUS_ID'
-            os.environ["CUDA_VISIBLE_DEVICES"] = set_free_gpus(num_gpus)
-    num_gpus = len(os.environ["CUDA_VISIBLE_DEVICES"])
-    print(os.environ["CUDA_VISIBLE_DEVICES"])
-    if gpus or num_gpus>0:
-        distribution = tf.contrib.distribute.MirroredStrategy(num_gpus=num_gpus)
-        config = tf.ConfigProto(allow_soft_placement=True)
-        config.gpu_options.allow_growth = True
-        return config
-    else:
-        return None
+class LateralConnection(tf.keras.layers.Layer):
 
+    def __init__(self, layer, data_set_size, n_samples, **kwargs):
+        super(LateralConnection, self).__init__(**kwargs)
+        self.layer = layer
+        self.data_set_size = data_set_size
+        self.n_samples = n_samples
+        self.from_client = clone(self.layer, data_set_size=self.data_set_size, n_samples=self.n_samples,
+                     activation='linear', name=self.name + '_from_client')
+        self.from_server = clone(self.layer, data_set_size=self.data_set_size, n_samples=self.n_samples,
+                     name=self.name + '_from_server1')
+        self.from_server_projection = clone(self.layer, data_set_size=self.data_set_size, n_samples=self.n_samples,
+                     activation='linear', name=self.name + '_from_server2')
+        self.gate = Gate()
+        self.layers = [self.from_client, self.from_server, self.from_server_projection, self.gate]
 
-def set_free_gpus(num):
-    # num: integer; number of GPUs that shall be allocated
-    # returns: string; listing a total of 'num' available GPUs.
+    def call(self, inputs):
+        out1 = self.from_client(inputs[0])
+        out2 = self.from_server(inputs[1])
+        out2 = self.from_server_projection(out2)
+        out2 = self.gate(out2)
+        outputs = tf.keras.layers.add([out1, out2], name=self.layer.name + '_add' + self.name)
+        outputs = tf.keras.layers.Activation(self.layer.get_config()['activation'],
+                                       name=self.layer.name + '_activation' + self.name)(outputs)
+        return outputs
 
-    list_gpu = GPUtil.getAvailable(limit=num, maxMemory=0.02)
-    return str(list_gpu)[1:-1]
+    @property
+    def weights(self):
+        weights = self.from_client.weights
+        weights.extend(self.from_server.weights)
+        weights.extend(self.from_server_projection.weights)
+        weights.extend(self.gate.weights)
+        return weights
+
+    def update_prior(self, kernel_prior_fn):
+        layers_to_update = [l for l in self.layers if hasattr(l, 'update_prior')]
+        for l, f in zip(layers_to_update, kernel_prior_fn):
+            l.update_prior(f)
 
 
 def multivariate_normal_fn(mu, u_sigma):
     def _fn(dtype, shape, name, trainable, add_variable_fn):
         del name, trainable, add_variable_fn, shape
-        scale = compute_scale(dtype, u_sigma)
+        scale = compute_scale(u_sigma)
         dist = tfd.Normal(loc=mu, scale=scale)
         batch_ndims = tf.size(input=dist.batch_shape_tensor())
         return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
@@ -100,24 +111,34 @@ def multivariate_normal_fn(mu, u_sigma):
     return _fn
 
 
-def compute_scale(dtype, u_sigma):
-    return np.finfo(dtype.as_numpy_dtype).eps + tf.nn.softplus(u_sigma)
+def compute_scale(u_sigma):
+    dtype = u_sigma.dtype
+    if hasattr(dtype, 'as_numpy_dtype'):
+        dtype = dtype.as_numpy_dtype
+    return np.finfo(dtype).eps + softplus(u_sigma)
 
 
 def compute_gaussian_ratio(mu1, u_sigma1, mu2, u_sigma2):
     def _fn(dtype, shape, name, trainable, add_variable_fn):
         del name, trainable, add_variable_fn, shape
-        scale1 = compute_scale(dtype, u_sigma1)
-        scale2 = compute_scale(dtype, u_sigma2)
-        scale = tf.math.reciprocal(tf.math.reciprocal(scale1) + tf.math.reciprocal(scale2))
-        mu = tf.math.multiply(scale,
-                              tf.math.multiply(tf.math.reciprocal(scale1), mu1) +
-                              tf.math.multiply(tf.math.reciprocal(scale2), mu2))
+        mu, scale = _gaussian_ratio_par(mu1, u_sigma1, mu2, u_sigma2)
         dist = tfd.Normal(loc=mu, scale=scale)
         batch_ndims = tf.size(input=dist.batch_shape_tensor())
         return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
 
     return _fn
+
+
+def softplus(x):
+    return np.log(np.exp(x) + 1)
+
+
+def softminus(x):
+    return np.log(np.exp(x) - 1)
+
+
+def softminus_tensor(x):
+    return tf.math.log(tf.math.exp(x) -1)
 
 
 def gaussian_ratio_par(v1, v2):
@@ -126,15 +147,29 @@ def gaussian_ratio_par(v1, v2):
         u_sigma1 = v1[1]
         mu2 = v2[0]
         u_sigma2 = v2[1]
-        scale1 = tf.nn.softplus(u_sigma1)
-        scale2 = tf.nn.softplus(u_sigma2)
-        scale = tf.math.reciprocal(tf.math.reciprocal(scale1) + tf.math.reciprocal(scale2))
-        mu = tf.math.multiply(scale,
-                              tf.math.multiply(tf.math.reciprocal(scale1), mu1) +
-                              tf.math.multiply(tf.math.reciprocal(scale2), mu2))
-        return mu, tf.math.log(tf.math.exp(scale)-1)
+        mu, scale = _gaussian_ratio_par(mu1, u_sigma1, mu2, u_sigma2)
+        return mu, softminus(scale)
     else:
         return []
+
+
+def _gaussian_ratio_par(mu1, u_sigma1, mu2, u_sigma2):
+    scale1 = compute_scale(u_sigma1)
+    scale2 = compute_scale(u_sigma2)
+    #scale1 = tf.debugging.check_numerics(scale1, 'error1')
+    #scale2 = tf.debugging.check_numerics(scale2, 'error2')
+    #with tf.control_dependencies([tf.assert_none_equal(scale1, scale2)]):
+    #scale = tf.math.reciprocal(np.finfo(scale1.dtype.as_numpy_dtype).eps + tf.math.reciprocal(scale1) -
+    #                           tf.math.reciprocal(scale2))
+    #scale = tf.debugging.check_numerics(scale, 'error')
+    #mu = tf.math.multiply(scale,
+    #                      tf.math.multiply(tf.math.reciprocal(scale1), mu1) -
+    #                      tf.math.multiply(tf.math.reciprocal(scale2), mu2))
+    print(scale1,scale2)
+    print(np.any(scale1>=scale2))
+    scale = 1/(np.finfo(scale1.dtype).eps + 1/scale1 - 1/scale2)
+    mu = scale*(1/scale1*mu1 - 1/scale2*mu2)
+    return mu, scale
 
 
 def get_refined_prior(l1, w2):
@@ -146,53 +181,15 @@ def get_refined_prior(l1, w2):
 
 
 def get_posterior_from_layer(l):
-    if l.get_weights():
-        return multivariate_normal_fn(l.get_weights()[0], l.get_weights()[1])
+    weights = l.get_weights()
+    if len(weights) > 1:
+        return multivariate_normal_fn(weights[0], weights[1])
     else:
-        return []
+        return
 
 
-def clone(layer, data_set_size=None, n_samples=None, **kwargs):
-    config = layer.get_config()
-    for key, value in kwargs.items():
-        if key == 'name':
-            config[key] = config[key] + value
-        else:
-            config[key] = value
-    if issubclass(layer.__class__, DenseReparameterization):
-        sub_config = dict((k, config[k]) for k in ('units', 'activation', 'name', 'activity_regularizer', 'trainable') if k in config)
-        keys_config = set(sub_config.keys())
-        kwargs.pop('name', None)
-        keys_kwargs = set(kwargs.keys())
-        union = keys_config | keys_kwargs
-        args = {}
-        for key in union:
-            if key in kwargs:
-                args[key] = kwargs[key]
-            else:
-                args[key] = sub_config[key]
-        args['kernel_divergence_fn'] = lambda q, p, _: tfp.distributions.kl_divergence(q, p)/(data_set_size*n_samples)
-        return layer.__class__(**args)
+def prior_wrapper(posterior, l):
+    if hasattr(l, 'layers'):
+        return [posterior(la) for la in l.layers]
     else:
-        return layer.__class__.from_config(config)
-
-
-def get_mlp_server(input_shape, layer, layer_units, activations, data_set_size, num_samples):
-    server = Server()
-    server.add(tf.keras.layers.InputLayer(input_shape=input_shape))
-    server.add(tf.keras.layers.Flatten())
-    for i, (u, act) in enumerate(zip(layer_units, activations)):
-        server.add(layer(u, activation=act, name='lateral' + str(i),
-              kernel_divergence_fn=
-              lambda q, p, _: tfp.distributions.kl_divergence(q, p)/(data_set_size*num_samples)))
-    return server
-
-
-def aulc(virtual_history, quantity='categorical_accuracy'):
-    history_first_pass = [v[0] for v in virtual_history.values()]
-    quantity_per_task = [h[quantity] for h in history_first_pass]
-    epochs = [len(q) for q in quantity_per_task]
-    min_epochs = min(epochs)
-    aulcs = [np.trapz(q[:min_epochs]) for q in quantity_per_task]
-
-    return np.array(aulcs)
+        return posterior(l)