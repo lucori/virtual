b5d2b1a17298e452db434bd5d39a2763fd36d716
lucac <luca.corinzia@inf.ethz.ch>
2020-02-21 18:36:03 +0100

    working Fedprox on shakespeare

--- source/test_LSTM.py
+++ source/test_LSTM.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from data_utils import post_process_datasets, federated_dataset
 from fed_prox import FedProx
 import tensorflow_federated as tff
-from centered_l2_regularizer import DenseCentered, CenteredL2Regularizer, LSTMCellCentered, RNNCentered, \
+from centered_layers import DenseCentered, CenteredL2Regularizer, LSTMCellCentered, RNNCentered, \
     EmbeddingCentered
 from utils import gpu_session
 import os
@@ -13,14 +13,14 @@ BUFFER_SIZE = 10000
 embedding_dim = 8
 rnn_units = 256
 VOCAB_SIZE = 86
-num_clients = 100
-num_rounds = 100
+num_clients = 10
+num_rounds = 1000
 clients_per_round = 5
-epochs_per_round = 2
-learning_rate = 0.1
+epochs_per_round = 1
+learning_rate = 0.001
 
-#gpu_session(1)
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+gpu_session(1)
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 vocab = list('dhlptx@DHLPTX $(,048cgkoswCGKOSW[_#\'/37;?bfjnrvzBFJNRVZ"&*.26:\naeimquyAEIMQUY]!%)-159\r')
 table = tf.lookup.StaticHashTable(
     tf.lookup.KeyValueTensorInitializer(
@@ -54,10 +54,14 @@ def preprocess(dataset):
 def postprocess(dataset):
     return (dataset.batch(SEQ_LENGTH + 1, drop_remainder=False)
                     # Shuffle and form minibatches
-                    .shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE, padded_shapes=[SEQ_LENGTH + 1], drop_remainder=False,
-                                                      padding_values=tf.cast(0, tf.int64))
-                    .padded_batch(1, padded_shapes=[BATCH_SIZE, SEQ_LENGTH + 1], padding_values=tf.cast(0, tf.int64),
-                                  drop_remainder=False).unbatch()
+                    .shuffle(BUFFER_SIZE).
+                    padded_batch(BATCH_SIZE, padded_shapes=[SEQ_LENGTH + 1],
+                                             drop_remainder=True,
+                                             padding_values=tf.cast(0, tf.int64))
+                    #.padded_batch(1, padded_shapes=[BATCH_SIZE, SEQ_LENGTH + 1],
+                    #              padding_values=tf.cast(0, tf.int64),
+                    #              drop_remainder=False)
+                    #.unbatch()
                     # And finally split into (input, target) tuples,
                     # each of length SEQ_LENGTH.
                     .map(split_input_target))
@@ -69,16 +73,30 @@ train_data, test_data = tff.simulation.datasets.shakespeare.load_data()
 def data(client, source):
     return postprocess(preprocess(source.create_tf_dataset_for_client(client)))
 
+indx = [8, 11,  12,  17,  26,  32,  34,  43,  45,  66,  68,  72,  73,
+           85,  92,  93,  98, 105, 106, 108, 110, 130, 132, 143, 150, 153,
+           156, 158, 165, 169, 185, 187, 191, 199, 207, 212, 219, 227, 235,
+           236, 238, 257, 264, 269, 278, 281, 283, 285, 288, 297, 301, 305,
+           310, 324, 331, 340, 351, 362, 370, 373, 374, 375, 376, 383, 388,
+           418, 428, 429, 432, 433, 458, 471, 474, 476, 485, 491, 492, 494,
+           497, 500, 501, 507, 512, 519, 529, 543, 556, 564, 570, 573, 574,
+           579, 580, 581, 593, 600, 601, 603, 604, 613, 622, 626, 627, 632,
+           644, 645, 646, 648, 657, 658, 660, 663, 669, 671, 672, 676, 678,
+           681, 684, 695]
 
-clients = train_data.client_ids[0:num_clients]
+clients = [train_data.client_ids[i] for i in indx]
 
+clients = clients[0:num_clients]
 
 train_size = [len(list(preprocess(train_data.create_tf_dataset_for_client(client)))) for client in clients]
 test_size = [len(list(preprocess(test_data.create_tf_dataset_for_client(client)))) for client in clients]
 print(train_size)
 print(test_size)
 clients, train_size, test_size = zip(*[(client, train_size[i], test_size[i])
-                                     for i, client in enumerate(clients) if train_size[i] > 0 and test_size[i] > 0])
+                                     for i, client in enumerate(clients) if
+                                       train_size[i] > (SEQ_LENGTH+1)*BATCH_SIZE and
+                                       test_size[i] > (SEQ_LENGTH+1)*BATCH_SIZE])
+
 num_clients = len(clients)
 federated_train_data = [data(client, train_data) for client in clients]
 federated_test_data = [data(client, test_data) for client in clients]
@@ -113,28 +131,26 @@ def model_fn(model_class, num_clients=1):
                           embeddings_regularizer=lambda:CenteredL2Regularizer(0.),
                           mask_zero=True),
         RNNCentered(LSTMCellCentered(rnn_units,
-                                             recurrent_initializer='glorot_uniform',
-                                             kernel_regularizer=lambda:CenteredL2Regularizer(0.),
-                                             recurrent_regularizer=lambda:CenteredL2Regularizer(0.),
-                                             bias_regularizer=lambda:CenteredL2Regularizer(0.)),
-                            return_sequences=True,
-                            stateful=True),
+                                     recurrent_initializer='glorot_uniform',
+                                     kernel_regularizer=lambda:CenteredL2Regularizer(0.),
+                                     recurrent_regularizer=lambda:CenteredL2Regularizer(0.),
+                                     bias_regularizer=lambda:CenteredL2Regularizer(0.)),
+                    return_sequences=True,
+                    stateful=True),
         RNNCentered(LSTMCellCentered(rnn_units,
-                                             recurrent_initializer='glorot_uniform',
-                                             kernel_regularizer=lambda:CenteredL2Regularizer(0.),
-                                             recurrent_regularizer=lambda:CenteredL2Regularizer(0.),
-                                             bias_regularizer=lambda:CenteredL2Regularizer(0.)),
-                            return_sequences=True,
-                            stateful=True),
+                                     recurrent_initializer='glorot_uniform',
+                                     kernel_regularizer=lambda:CenteredL2Regularizer(0.),
+                                     recurrent_regularizer=lambda:CenteredL2Regularizer(0.),
+                                     bias_regularizer=lambda:CenteredL2Regularizer(0.)),
+                    return_sequences=True,
+                    stateful=True),
         DenseCentered(VOCAB_SIZE+1, kernel_regularizer=lambda:CenteredL2Regularizer(0.),
                       bias_regularizer=lambda:CenteredL2Regularizer(0.))
     ])
     return compile(keras_model)
 
 
-
-logdir = 'logs/LSTM'
-
+logdir = 'logs/LSTM_1'
 
 fed_prox_process = FedProx(model_fn, num_clients)
 fed_prox_process.fit(federated_train_data, num_rounds, clients_per_round,
