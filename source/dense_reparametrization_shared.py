360892dde327e0260c12cfe12554a7bada50e282
lucac <luca.corinzia@inf.ethz.ch>
2020-02-24 18:13:59 +0100

    generic reparemetrizaiton class

--- source/dense_reparametrization_shared.py
+++ source/dense_reparametrization_shared.py
@@ -5,12 +5,46 @@ from tfp_utils import renormalize_mean_field_normal_fn, default_tensor_multivari
             compute_gaussian_ratio, compute_gaussian_prod, loc_ratio_from_precision
 from tensorflow_probability.python import distributions as tfd
 from tensorflow_probability.python.layers import util as tfp_layers_util
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras.layers.recurrent import _caching_device
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.python.ops import array_ops
 
 
 inf = 1e15
 softplus = tfp.bijectors.Softplus()
 precision_from_untransformed_scale = tfp.bijectors.Chain([precision_from_scale, softplus])
 
+
+class VariationalReparametrized(tf.keras.layers.Layer):
+
+    def build_posterior_fn(self, in_size, dtype):
+        self.s_loc = self.add_variable(name='s_loc', shape=[in_size, self.units], dtype=dtype, trainable=False,
+                                       initializer=tf.random_normal_initializer(stddev=0.1 * math.sqrt(self.num_clients)))
+        scale_init = tf.random_normal_initializer(mean=+inf, stddev=0.1).__call__(shape=[in_size, self.units])
+        scale_init = softplus.inverse(softplus.forward(scale_init) / math.sqrt(self.num_clients))
+        s_untransformed_scale = self.add_variable(name='s_untransformed_scale', shape=[in_size, self.units], dtype=dtype,
+                                                  trainable=False,
+                                                  initializer=tf.keras.initializers.constant(scale_init.numpy()))
+        self.s_prec = tfp.util.DeferredTensor(s_untransformed_scale, precision_from_untransformed_scale)
+
+        self.s_i_loc = self.add_variable(name='s_i_loc', shape=[in_size, self.units], dtype=dtype, trainable=False,
+                                         initializer=tf.random_normal_initializer(stddev=0.1))
+        s_i_untransformed_scale = self.add_variable(name='s_i_untransformed_scale', shape=[in_size, self.units],
+                                                    dtype=dtype,
+                                                    trainable=False,
+                                                    initializer=tf.random_normal_initializer(mean=+inf, stddev=0.1))
+        self.s_i_prec = tfp.util.DeferredTensor(s_i_untransformed_scale, precision_from_untransformed_scale)
+        self.prec_ratio = tfp.util.DeferredTensor(self.s_prec, lambda x: x - self.s_i_prec)
+
+        def loc_reparametrization_fn(x):
+            return loc_ratio_from_precision(x, self.s_prec, self.s_i_loc, self.s_i_prec)
+
+        self.loc_ratio = tfp.util.DeferredTensor(self.s_loc, loc_reparametrization_fn)
+        self.kernel_posterior_fn = self.kernel_posterior_fn(self.loc_ratio, self.prec_ratio)
+        self.kernel_prior_fn = self.kernel_prior_fn(self.loc_ratio, self.prec_ratio, self.num_clients, self.prior_scale)
+        return kernel_posterior_fn, kernel_prior_fn
 
 class DenseReparametrizationShared(tfp.layers.DenseReparameterization):
 
@@ -143,3 +177,116 @@ class DenseReparametrizationShared(tfp.layers.DenseReparameterization):
         self.variables[0].assign(loc)
         self.variables[1].assign(precision_from_untransformed_scale.inverse(prec))
 
+
+class LSTMCellVariational(tf.keras.layers.LSTMCell):
+
+    def __init__(self,
+                 units,
+                 activation='tanh',
+                 recurrent_activation='hard_sigmoid',
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 bias_initializer='zeros',
+                 unit_forget_bias=True,
+                 kernel_constraint=None,
+                 recurrent_constraint=None,
+                 bias_constraint=None,
+                 dropout=0.,
+                 recurrent_dropout=0.,
+                 implementation=1,
+                 kernel_posterior_fn=renormalize_mean_field_normal_fn,
+                 kernel_posterior_tensor_fn=(lambda d: d.sample()),
+                 recurrent_kernel_posterior_fn=renormalize_mean_field_normal_fn,
+                 recurrent_kernel_posterior_tensor_fn=(lambda d: d.sample()),
+                 kernel_prior_fn=default_tensor_multivariate_normal_fn,
+                 kernel_divergence_fn=(lambda q, p, ignore: tfd.kl_divergence(q, p)),
+                 bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(
+                     is_singular=True),
+                 bias_posterior_tensor_fn=(lambda d: d.sample()),
+                 bias_prior_fn=None,
+                 bias_divergence_fn=(lambda q, p, ignore: tfd.kl_divergence(q, p)),
+                 **kwargs):
+
+        super(LSTMCellVariational, self).__init__(units,
+                                                  activation=activation,
+                                                  recurrent_activation=recurrent_activation,
+                                                  use_bias=use_bias,
+                                                  kernel_initializer=kernel_initializer,
+                                                  recurrent_initializer=recurrent_initializer,
+                                                  bias_initializer=bias_initializer,
+                                                  unit_forget_bias=unit_forget_bias,
+                                                  kernel_regularizer=None,
+                                                  recurrent_regularizer=None,
+                                                  bias_regularizer=None,
+                                                  kernel_constraint=kernel_constraint,
+                                                  recurrent_constraint=recurrent_constraint,
+                                                  bias_constraint=bias_constraint,
+                                                  dropout=dropout,
+                                                  recurrent_dropout=recurrent_dropout,
+                                                  implementation=implementation,
+                                                  **kwargs)
+        self.kernel_posterior_fn = kernel_posterior_fn
+        self.kernel_posterior_tensor_fn = kernel_posterior_tensor_fn
+        self.recurrent_kernel_posterior_fn = recurrent_kernel_posterior_fn
+        self.recurrent_kernel_posterior_tensor_fn = recurrent_kernel_posterior_tensor_fn
+        self.kernel_posterior_tensor_fn = kernel_posterior_tensor_fn
+        self.kernel_prior_fn = kernel_prior_fn
+        self.kernel_divergence_fn = kernel_divergence_fn
+        self.bias_posterior_fn = bias_posterior_fn
+        self.bias_posterior_tensor_fn = bias_posterior_tensor_fn
+        self.bias_prior_fn = bias_prior_fn
+        self.bias_divergence_fn = bias_divergence_fn
+
+    @tf_utils.shape_type_conversion
+    def build(self, input_shape):
+        default_caching_device = _caching_device(self)
+        input_dim = input_shape[-1]
+
+        self.kernel = self.add_weight(
+            shape=(input_dim, self.units * 4),
+            name='kernel',
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
+            caching_device=default_caching_device)
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 4),
+            name='recurrent_kernel',
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint,
+            caching_device=default_caching_device)
+
+        if self.use_bias:
+            if self.unit_forget_bias:
+
+                def bias_initializer(_, *args, **kwargs):
+                    return K.concatenate([
+                        self.bias_initializer((self.units,), *args, **kwargs),
+                        initializers.Ones()((self.units,), *args, **kwargs),
+                        self.bias_initializer((self.units * 2,), *args, **kwargs),
+                        ])
+            else:
+                bias_initializer = self.bias_initializer
+            self.bias_regularizer.center = self.add_weight('bias_center',
+                                                           shape=(self.units * 4,),
+                                                           initializer=tf.keras.initializers.constant(0.),
+                                                           dtype=self.dtype,
+                                                           trainable=False)
+            self.bias = self.add_weight(
+                          shape=(self.units * 4,),
+                          name='bias',
+                          initializer=bias_initializer,
+                          regularizer=self.bias_regularizer,
+                          constraint=self.bias_constraint,
+                          caching_device=default_caching_device)
+        else:
+            self.bias = None
+        self.built = True
+
+    def call(self, inputs, states, training=None):
+        self.kernel = self.kernel_posterior_fn(self.kernel_posterior)
+        self.recurrent_kernel = self.recurrent_kernel_fn(self.kernel_posterior)
+        ##todo: add variational loss
+        return super(LSTMCellVariational, self).call(inputs, states, training)
