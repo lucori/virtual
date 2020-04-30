685d6a69b11aa9e306b729e4d76ba692915a88a1
Ami <abahrein@ethz.ch>
2020-04-30 11:34:24 +0200

    refactor fedprocess models

--- source/fed_prox.py
+++ source/fed_prox.py
@@ -1,5 +1,7 @@
 import random
 import logging
+from pathlib import Path
+
 import tensorflow as tf
 
 from source.federated_devices import ClientSequential, ServerSequential
@@ -18,47 +20,62 @@ class FedProx(FedProcess):
         self.clients = None
 
     def build(self, *args, **kwargs):
-        self.clients = [self.model_fn(ClientSequential, 1) for _ in range(self.num_clients)]
+        self.clients = [self.model_fn(ClientSequential, 1)
+                        for _ in range(self.num_clients)]
         self.server = self.model_fn(ServerSequential, 1)
 
-    def fit(self, federated_train_data, num_rounds, clients_per_round, epochs_per_round, federated_test_data=None,
-            tensorboard_updates=1, logdir='', callbacks=None, train_size=None, test_size=None):
+    def fit(self, federated_train_data, num_rounds, clients_per_round,
+            epochs_per_round, federated_test_data=None,
+            tensorboard_updates=1, logdir=Path(), callbacks=None,
+            train_size=None, test_size=None):
 
         train_log_dir = logdir / 'train'
-        self.train_summary_writer = tf.summary.create_file_writer(str(train_log_dir))
+        self.train_summary_writer = \
+            tf.summary.create_file_writer(str(train_log_dir))
         self.test_summary_writer = tf.summary.create_file_writer(str(logdir))
 
         self.build()
 
-        for round in range(num_rounds):
+        for round_i in range(num_rounds):
 
             deltas = []
             history_train = []
-
-            clients_sampled = random.sample(self.clients_indx, clients_per_round)
+            clients_sampled = random.sample(self.clients_indx,
+                                            clients_per_round)
             for indx in clients_sampled:
+
                 self.clients[indx].receive_and_save_weights(self.server)
                 self.clients[indx].renew_center()
-                history_single = self.clients[indx].fit(federated_train_data[indx], verbose=0,
-                                                 epochs=epochs_per_round, callbacks=callbacks)
-                history_train.append({key: history_single.history[key] for key in history_single.history.keys()
+                history_single = self.clients[indx].fit(
+                    federated_train_data[indx],
+                    verbose=0,
+                    epochs=epochs_per_round,
+                    callbacks=callbacks)
+
+                history_train.append({key: history_single.history[key]
+                                      for key in history_single.history.keys()
                                       if 'val' not in key})
                 delta = self.clients[indx].compute_delta()
                 deltas.append(delta)
 
-            aggregated_deltas = self.aggregate_deltas_multi_layer(deltas,
-                                                                  [train_size[client]/sum(train_size)
-                                                                   for client in clients_sampled])
+            aggregated_deltas = self.aggregate_deltas_multi_layer(
+                deltas, [train_size[client]/sum(train_size)
+                         for client in clients_sampled])
+
             self.server.apply_delta(aggregated_deltas)
-            test = [self.server.evaluate(test_data, verbose=0) for test_data in federated_test_data]
-            avg_train = avg_dict(history_train, [train_size[client] for client in clients_sampled])
-            avg_test = avg_dict_eval(test, [size/sum(test_size) for size in test_size])
-            logger.info(f'round: {round}, '
+            test = [self.server.evaluate(test_data, verbose=0)
+                    for test_data in federated_test_data]
+            avg_train = avg_dict(history_train,
+                                 [train_size[client]
+                                  for client in clients_sampled])
+            avg_test = avg_dict_eval(test, [size/sum(test_size)
+                                            for size in test_size])
+            logger.info(f'round: {round_i}, '
                         f'avg_train: {avg_train}, '
                         f'avg_test: {avg_test}')
-            if round % tensorboard_updates == 0:
+            if round_i % tensorboard_updates == 0:
                 for i, key in enumerate(avg_train.keys()):
                     with self.train_summary_writer.as_default():
-                        tf.summary.scalar(key, avg_train[key], step=round)
+                        tf.summary.scalar(key, avg_train[key], step=round_i)
                     with self.test_summary_writer.as_default():
-                        tf.summary.scalar(key, avg_test[i], step=round)
+                        tf.summary.scalar(key, avg_test[i], step=round_i)
--- source/virtual_process.py
+++ source/virtual_process.py
@@ -1,7 +1,7 @@
 import random
 import logging
+from pathlib import Path
 
-import os
 import tensorflow as tf
 
 from source.federated_devices import (ClientVirtualSequential,
@@ -12,13 +12,13 @@ from source.fed_process import FedProcess
 from source.utils import avg_dict
 from source.constants import ROOT_LOGGER_STR
 
-
 logger = logging.getLogger(ROOT_LOGGER_STR + '.' + __name__)
 
 
 class VirtualFedProcess(FedProcess):
 
-    def __init__(self, model_fn, num_clients, damping_factor=1, fed_avg_init=True):
+    def __init__(self, model_fn, num_clients, damping_factor=1,
+                 fed_avg_init=True):
         super(VirtualFedProcess, self).__init__(model_fn, num_clients)
         self.damping_factor = damping_factor
         self.fed_avg_init = fed_avg_init
@@ -35,53 +35,67 @@ class VirtualFedProcess(FedProcess):
             self.clients.append(model)
         self.server = self.model_fn(server_model_class, 1)
 
-    def fit(self, federated_train_data, num_rounds, clients_per_round, epochs_per_round, federated_test_data=None,
-            tensorboard_updates=1, logdir='', callbacks=None, train_size=None, test_size=None, hierarchical=False):
+    def fit(self, federated_train_data, num_rounds, clients_per_round,
+            epochs_per_round, federated_test_data=None,
+            tensorboard_updates=1, logdir=Path(), callbacks=None,
+            train_size=None, test_size=None, hierarchical=False):
 
-        train_log_dir = os.path.join(logdir, 'train')
-        self.train_summary_writer = tf.summary.create_file_writer(str(train_log_dir))
+        train_log_dir = logdir / 'train'
+        self.train_summary_writer = \
+            tf.summary.create_file_writer(str(train_log_dir))
         self.test_summary_writer = tf.summary.create_file_writer(str(logdir))
 
         self.build(train_size, hierarchical)
 
         history_test = [None] * len(self.clients)
-        for round in range(num_rounds):
+        for round_i in range(num_rounds):
 
             deltas = []
             history_train = []
 
-            clients_sampled = random.sample(self.clients_indx, clients_per_round)
+            clients_sampled = random.sample(self.clients_indx,
+                                            clients_per_round)
             for indx in clients_sampled:
                 self.clients[indx].receive_and_save_weights(self.server)
                 self.clients[indx].renew_center()
-                if round > 0 and self.fed_avg_init:
+                if round_i > 0 and self.fed_avg_init:
                     self.clients[indx].initialize_kernel_posterior()
 
-                history_single = self.clients[indx].fit(federated_train_data[indx], verbose=0,
-                                                        validation_data=federated_test_data[indx],
-                                                        epochs=epochs_per_round, callbacks=callbacks)
-                history_train.append({key: history_single.history[key] for key in history_single.history.keys()
+                history_single = self.clients[indx].fit(
+                    federated_train_data[indx],
+                    verbose=0,
+                    validation_data=federated_test_data[indx],
+                    epochs=epochs_per_round,
+                    callbacks=callbacks)
+
+                history_train.append({key: history_single.history[key]
+                                      for key in history_single.history.keys()
                                       if 'val' not in key})
-                history_test[indx] = {key.replace('val_', ''): history_single.history[key] for key in
-                                      history_single.history.keys() if 'val' in key}
+                history_test[indx] = \
+                    {key.replace('val_', ''): history_single.history[key]
+                     for key in history_single.history.keys()
+                     if 'val' in key}
+
                 self.clients[indx].apply_damping(self.damping_factor)
                 delta = self.clients[indx].compute_delta()
                 deltas.append(delta)
 
             aggregated_deltas = self.aggregate_deltas_multi_layer(deltas)
             self.server.apply_delta(aggregated_deltas)
-            avg_train = avg_dict(history_train, [train_size[client] for client in clients_sampled])
+            avg_train = avg_dict(history_train,
+                                 [train_size[client]
+                                  for client in clients_sampled])
             avg_test = avg_dict(history_test, test_size)
-            logger.debug(f"round: {round}, "
+            logger.debug(f"round: {round_i}, "
                          f"avg_train: {avg_train}, "
                          f"avg_test: {avg_test}")
-            if round % tensorboard_updates == 0:
+            if round_i % tensorboard_updates == 0:
                 with self.train_summary_writer.as_default():
                     for key in avg_train.keys():
-                        tf.summary.scalar(key, avg_train[key], step=round)
+                        tf.summary.scalar(key, avg_train[key], step=round_i)
                 with self.test_summary_writer.as_default():
                     for key in avg_test.keys():
-                        tf.summary.scalar(key, avg_test[key], step=round)
+                        tf.summary.scalar(key, avg_test[key], step=round_i)
 
         for i, client in enumerate(self.clients):
             client.save_weights(str(logdir / f'weights_{i}.h5'))
